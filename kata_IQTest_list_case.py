0154d53d8d397859f1f17875b0338b435d96913e
Joyce Rigelo <jrigelo@utexas.edu>
2016-04-21 19:32:50 -0500

    better comments

--- kata_IQTest_list_case.py
+++ kata_IQTest_list_case.py
@@ -11,22 +11,22 @@ def iqTest(numbers):
   # create lists for odd and even values
   odd = []
   even = []
+  # separate odd and even numbers
   for index in range(0,len(numbers)):
     number = int(numbers[index])
     if number%2 == 0:
       even.append((number, index))
     else:
       odd.append((number, index))
+  #check which list is bigger (than 1)
   if len(even) > len(odd):
     result = odd[0][1]
   else:
     result = even[0][1]
+  #add one to give position
   return result + 1
 
-
-
 def main():
-  #result = iqTest(" 2 2 7 6 ")
   result = iqTest(" 1 3 7 14 10")
   print "=>", result
 
